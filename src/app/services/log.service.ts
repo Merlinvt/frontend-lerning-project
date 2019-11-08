c55f089e307e588cc901b7724881b564e2cbbdd8
Merlin von Trott <mt@fino.digital>
2019-11-08 11:00:20 +0100

    ?

---
+++ src/app/services/log.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+import { Observable } from 'rxjs';
+import { of } from 'rxjs';
+
+import { Log } from '../models/log';
+import { from } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogService {
+  logs: Log[];
+
+  private logSource = new BehaviorSubject<Log>({id: null, text: null, date: null});
+  selectedLog = this.logSource.asObservable();
+
+  constructor() {
+    this.logs = [
+      {id: '1', text: 'Generated Components', date: new Date
+      ('12/24/2019 12:54:23')},
+      {id: '2', text: 'Added login', date: new Date
+      ('12/24/2019 12:54:23')},
+      {id: '3', text: 'Some stuf', date: new Date
+      ('12/24/2019 12:54:23')},
+    ];
+  }
+
+  getLogs(): Observable<Log[]> {
+    return of(this.logs);
+  }
+
+  setFormLog(log: Log) {
+    this.logSource.next(log)
+  }
+}
